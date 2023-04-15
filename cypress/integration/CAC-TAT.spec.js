f43eb9657a1c553b1cf040336926d0e595230fcc
FLPBaruch <lipepitanga1993@gmail.com>
2023-04-14 21:08:55 -0300

    updates dos exercicios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -48,7 +48,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#lastName').type('Baruch')
         cy.get('#email').type('f.baruch@dino.com.br')
         cy.get('#open-text-area').type('Este é um exercicio do curso de cypress', {delay: 0})
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('.button').click()
         cy.get('.error > strong').should('be.visible')
 
@@ -83,10 +83,44 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#product').select('YouTube').should('have.value', 'youtube')
     })
 
-    it.only('marca o tipo de atendimento "Feedback"', function() {
+    it('marca o tipo de atendimento "Feedback"', function() {
 
         cy.get('#support-type > :nth-child(2) > input').check().should('be.checked')
         cy.get(':nth-child(4) > input').check().should('be.checked')
         cy.get(':nth-child(3) > input').check().should('be.checked')
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+
+        cy.get('#check input[type="checkbox"]').as('checkboxes').check()
+
+        cy.get('@checkboxes').each(checkbox => {expect(checkbox[0].checked).to.equal(true)})
+
+        cy.get('#check input[type="checkbox"]').last().uncheck()
+
+        cy.get('#check input[type="checkbox"]').last().should('not.be.checked')
+
+    })
+
+    it('seleciona um arquivo da pasta fixtures', function() {
+
+        cy.get('input[type="file"]').should('not.have.value').selectFile('./cypress/fixtures/example.json').should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+
+        cy.get('input[type="file"]').should('not.have.value').selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'}).should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]').selectFile('@sampleFile').should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
         })
   })
